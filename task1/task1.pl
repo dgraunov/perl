9c876e23082af9497cddef2f59e6d90b5784f6f4
Graunov <dgraunov@list.ru>
2023-03-13 20:06:31 +0300

    add task1

---
+++ task1/task1.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my @users_list = ( 'dmitry', 'vasya', 'lolik' );
+print "Введите имя пользователя:\n";
+my $user_name = <STDIN>;
+chomp $user_name;
+my $find = 0;
+
+for my $user ( @users_list ) {
+    if ( $user_name eq $user ) { $find = 1 }
+ }
+
+if ( $find eq 1 ){
+   print "Добро пожаловать, $user_name!\n"
+}else {
+   print "Ты кто такой, $user_name?\n"
+   }
